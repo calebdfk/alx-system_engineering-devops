15def0f2f0bcb6ae5394ab11a4cb2294bb187425
calebdfk <dkweteza@outlook.com>
2024-03-22 14:02:44 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# installs the package puppet-lint
+# Install package with flask
+
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
